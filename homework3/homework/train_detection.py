efb5809ebb194740a9e2f7fdd6ae66095f94abdd
Taiwo Momoh <taiwoladipupo@gmail.com>
2025-03-24 18:41:39 -0500

    Adding detector

--- homework3/homework/train_detection.py
+++ homework3/homework/train_detection.py
@@ -94,7 +94,7 @@ class CombinedLoss(nn.Module):
         self.seg_loss_weight = seg_loss_weight
         self.depth_loss_weight = depth_loss_weight
         # Set class weights: background=0.1, classes 1 & 2=20.0
-        self.register_buffer("class_weights", torch.tensor([0.1, 20.0, 20.0], dtype=torch.float32))
+        self.register_buffer("class_weights", torch.tensor([1.0, 5.0, 5.0], dtype=torch.float32))
         if device is not None:
             self.class_weights = self.class_weights.to(device)
         if self.use_focal_loss:
