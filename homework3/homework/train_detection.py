4d75fe78415c23e25e09b135d8a68d4b22eccaf3
Taiwo Momoh <taiwoladipupo@gmail.com>
2025-03-26 06:05:55 -0500

    Adding detector

--- homework3/homework/train_detection.py
+++ homework3/homework/train_detection.py
@@ -90,10 +90,12 @@ def train(
                    "target_indices": target_indices.shape,})
 
             # Ensure the tensors have compatible dimensions
+            # Ensure the tensors have compatible dimensions
             if pred_depth.shape[2] != depth.shape[2]:
+                if depth.dim() == 3:  # If depth has only 3 dimensions, add a dimension
+                    depth = depth.unsqueeze(1)
                 pred_depth = F.interpolate(pred_depth, size=(depth.shape[2], depth.shape[3]), mode='bilinear',
                                            align_corners=False)
-
             training_metrics.add(pred_labels, track,pred_depth, depth)
 
             loss = alpha * ce_loss(pred, target_indices) + beta * mse_loss(pred_depth, depth)
