6ba5309af654ddbb80ca17d2393503554674eedb
Taiwo Momoh <taiwoladipupo@gmail.com>
2025-04-05 11:59:55 -0500

    Added homework4

---
+++ homework4/homework/models.py
@@ -0,0 +1,229 @@
+from pathlib import Path
+
+import torch
+import torch.nn as nn
+
+
+HOMEWORK_DIR = Path(__file__).resolve().parent
+INPUT_MEAN = [0.2788, 0.2657, 0.2629]
+INPUT_STD = [0.2064, 0.1944, 0.2252]
+
+
+class MLPPlanner(nn.Module):
+    def __init__(
+        self,
+        n_track: int = 10,
+        n_waypoints: int = 3,
+    ):
+        """
+        Args:
+            n_track (int): number of points in each side of the track
+            n_waypoints (int): number of waypoints to predict
+        """
+        super().__init__()
+        # Define Input dim
+        # Each input b has 2 coordinates;
+        input_dim = n_track * 4 # 2 sides, each with 2 coordinates
+        hidden_dim = 256 # Assuming
+        output_dim = n_waypoints * 2 # 2 coordinates for each waypoint
+
+
+        self.n_track = n_track
+        self.n_waypoints = n_waypoints
+
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+
+
+    def forward(
+        self,
+        track_left: torch.Tensor,
+        track_right: torch.Tensor,
+        **kwargs,
+    ) -> torch.Tensor:
+        """
+        Predicts waypoints from the left and right boundaries of the track.
+
+        During test time, your model will be called with
+        model(track_left=..., track_right=...), so keep the function signature as is.
+
+        Args:
+            track_left (torch.Tensor): shape (b, n_track, 2)
+            track_right (torch.Tensor): shape (b, n_track, 2)
+
+        Returns:
+            torch.Tensor: future waypoints with shape (b, n_waypoints, 2)
+        """
+        # Concatenate the left and right track points
+        x = torch.cat((track_left, track_right), dim=2)
+        # Flatten the input
+        x = x.view(x.size(0), -1)  # shape (b, n_track * 4)
+        # Pass through the fc1 with relu activation
+        x = torch.relu(self.fc1(x))
+        # Pass through the fc2
+        out = self.fc2(x)
+        # Reshape the output to (b, n_waypoints, 2)
+        out = out.view(-1, self.n_waypoints, 2)
+        return out
+
+class TransformerPlanner(nn.Module):
+    def __init__(
+        self,
+        n_track: int = 10,
+        n_waypoints: int = 3,
+        d_model: int = 64,
+    ):
+        super().__init__()
+
+        self.n_track = n_track
+        self.n_waypoints = n_waypoints
+        # Learned query embeddings for each waypoints
+        self.query_embed = nn.Embedding(n_waypoints, d_model)
+        # Linear encoder to project each track point to a higher dimension
+        self.transformer_encoder = nn.Linear(2, d_model)
+        #Build aa transformer decoder: a single decoder with 4 attention heads
+
+        decoder_layer = nn.TransformerDecoderLayer(d_model=d_model, nhead=4)
+        # Build the transformer decoder
+        self.transformer_decoder = nn.TransformerDecoder(decoder_layer, num_layers=2)
+
+        # Final layer
+        self.out_proj = nn.Linear(d_model, 2)
+
+
+
+
+
+
+    def forward(
+        self,
+        track_left: torch.Tensor,
+        track_right: torch.Tensor,
+        **kwargs,
+    ) -> torch.Tensor:
+        """
+        Predicts waypoints from the left and right boundaries of the track.
+
+        During test time, your model will be called with
+        model(track_left=..., track_right=...), so keep the function signature as is.
+
+        Args:
+            track_left (torch.Tensor): shape (b, n_track, 2)
+            track_right (torch.Tensor): shape (b, n_track, 2)
+
+        Returns:
+            torch.Tensor: future waypoints with shape (b, n_waypoints, 2)
+        """
+        # concatenate the left and right track points
+        track_points = torch.cat((track_left, track_right), dim=2)  # shape (b, n_track, 4)
+
+        # Encode each track points into d_model features
+        track_embedded = self.transformer_encoder(track_points)  # shape (b, n_track, d_model)
+
+        # Permute track_embedded to (sequence_length, b, d_model) for transformer decoder
+        track_embedded = track_embedded.permute(1, 0, 2)  # shape (n_track, b, d_model)
+        # get the query embeddings for the waypoints
+        # shape (n_waypoints, b, d_model)
+        queries = self.query_embed.weight
+        batch_size = track_left.size(0)
+        queries = queries.unsqueeze(1).expand(-1, batch_size, -1)  # shape (n_waypoints, b, d_model)
+
+        # Pass through the transformer decoder
+        decoded = self.transformer_decoder(queries, track_embedded)
+
+        # Permute the decoded outputs to (b, n_waypoints, d_model)
+        decoded = decoded.permute(1, 0, 2)  # shape (b, n_waypoints, d_model)
+        # Pass through the final layer to get the waypoints
+        waypoints = self.out_proj(decoded)
+        return waypoints
+
+
+
+class CNNPlanner(torch.nn.Module):
+    def __init__(
+        self,
+        n_waypoints: int = 3,
+    ):
+        super().__init__()
+
+        self.n_waypoints = n_waypoints
+
+        self.register_buffer("input_mean", torch.as_tensor(INPUT_MEAN), persistent=False)
+        self.register_buffer("input_std", torch.as_tensor(INPUT_STD), persistent=False)
+
+    def forward(self, image: torch.Tensor, **kwargs) -> torch.Tensor:
+        """
+        Args:
+            image (torch.FloatTensor): shape (b, 3, h, w) and vals in [0, 1]
+
+        Returns:
+            torch.FloatTensor: future waypoints with shape (b, n, 2)
+        """
+        x = image
+        x = (x - self.input_mean[None, :, None, None]) / self.input_std[None, :, None, None]
+
+        raise NotImplementedError
+
+
+MODEL_FACTORY = {
+    "mlp_planner": MLPPlanner,
+    "transformer_planner": TransformerPlanner,
+    "cnn_planner": CNNPlanner,
+}
+
+
+def load_model(
+    model_name: str,
+    with_weights: bool = False,
+    **model_kwargs,
+) -> torch.nn.Module:
+    """
+    Called by the grader to load a pre-trained model by name
+    """
+    m = MODEL_FACTORY[model_name](**model_kwargs)
+
+    if with_weights:
+        model_path = HOMEWORK_DIR / f"{model_name}.th"
+        assert model_path.exists(), f"{model_path.name} not found"
+
+        try:
+            m.load_state_dict(torch.load(model_path, map_location="cpu"))
+        except RuntimeError as e:
+            raise AssertionError(
+                f"Failed to load {model_path.name}, make sure the default model arguments are set correctly"
+            ) from e
+
+    # limit model sizes since they will be zipped and submitted
+    model_size_mb = calculate_model_size_mb(m)
+
+    if model_size_mb > 20:
+        raise AssertionError(f"{model_name} is too large: {model_size_mb:.2f} MB")
+
+    return m
+
+
+def save_model(model: torch.nn.Module) -> str:
+    """
+    Use this function to save your model in train.py
+    """
+    model_name = None
+
+    for n, m in MODEL_FACTORY.items():
+        if type(model) is m:
+            model_name = n
+
+    if model_name is None:
+        raise ValueError(f"Model type '{str(type(model))}' not supported")
+
+    output_path = HOMEWORK_DIR / f"{model_name}.th"
+    print(f"Saving model to {output_path.name}")
+    torch.save(model.state_dict(), output_path)
+
+    return output_path
+
+
+def calculate_model_size_mb(model: torch.nn.Module) -> float:
+    """
+    Naive way to estimate model size
+    """
+    return sum(p.numel() for p in model.parameters()) * 4 / 1024 / 1024
---
+++ homework4/homework/train_planner.py
@@ -0,0 +1,136 @@
+import argparse
+from datetime import datetime
+from pathlib import Path
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.utils.tensorboard as tb
+from torch import nn
+from .metrics import PlannerMetric
+from .models import load_model, save_model
+from .datasets.road_dataset import load_data
+
+
+def train(exp_dir: str = "logs",
+          model_name: str = "mlp_planner",
+          num_epoch: int = 50,
+          lr: float = 1e-3,
+          batch_size: int = 128,
+          seed: int = 2024,
+          **kwargs):
+    # Set device.
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available() and torch.backends.mps.is_built():
+        device = torch.device("mps")
+    else:
+        print("CUDA not available, using CPU")
+        device = torch.device("cpu")
+
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    log_dir = Path(exp_dir) / f"{model_name}_{datetime.now().strftime('%m%d_%H%M%S')}"
+    logger = tb.SummaryWriter(log_dir)
+
+    model = load_model(model_name, **kwargs)
+    model = model.to(device)
+    model.train()
+
+    train_data = load_data("drive_data/train", shuffle=True, batch_size=batch_size, num_workers=2)
+    val_data = load_data("drive_data/val", shuffle=False)
+
+    loss_fn = nn.MSELoss()  # Assuming the loss function is MSE for regression tasks
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    global_step = 0
+    train_metrics = PlannerMetric()
+    val_metrics = PlannerMetric()
+
+    for epoch in range(num_epoch):
+        # Reset Metrics at beginning of each epoch
+        train_metrics.reset()
+        val_metrics.reset()
+
+        for batch in train_data:
+            batch = {k: (v.to(device) if isinstance(v, torch.Tensor) else v) for k, v in batch.items()}
+            track_left = batch["track_left"]
+            track_right = batch["track_right"]
+            waypoints = batch["waypoints"]
+
+            optimizer.zero_grad()
+            # forward pass
+            pred = model(track_left, track_right)
+            # compute loss
+            loss = loss_fn(pred, waypoints)
+            loss.backward()
+            optimizer.step()
+
+            # Update metrics
+            train_metrics.add(pred, waypoints)
+
+            global_step += 1
+
+        # Evaluation mode
+
+        with torch.inference_mode():
+            model.eval()
+
+            for batch in val_data:
+                batch = {k: (v.to(device) if isinstance(v, torch.Tensor) else v) for k, v in batch.items()}
+                track_left = batch["track_left"]
+                track_right = batch["track_right"]
+                waypoints = batch["waypoints"]
+
+                pred = model(track_left, track_right)
+
+                # Update metrics
+                val_metrics.add(pred, waypoints)
+
+        training_metrics = train_metrics.compute()
+        validation_metrics = val_metrics.compute()
+
+        # Compute longitudinal and lateral errors
+        train_l1_error = training_metrics["l1_error"]
+        train_longitudinal_error = training_metrics["longitudinal_error"]
+        train_lateral_error = training_metrics["lateral_error"]
+
+        # Log metrics
+        logger.add_scalar("train/l1_error", train_l1_error, global_step)
+        logger.add_scalar("train/longitudinal_error", train_longitudinal_error, global_step)
+        logger.add_scalar("train/lateral_error", train_lateral_error, global_step)
+
+        val_l1_error = validation_metrics["l1_error"]
+        val_longitudinal_error = validation_metrics["longitudinal_error"]
+        val_lateral_error = validation_metrics["lateral_error"]
+
+        logger.add_scalar("val/l1_error", val_l1_error, global_step)
+        logger.add_scalar("val/longitudinal_error", val_longitudinal_error, global_step)
+        logger.add_scalar("val/lateral_error", val_lateral_error, global_step)
+
+        # Print metrics
+        print(f"Epoch {epoch + 1}/{num_epoch}, - "
+              f"Train L1 Error: {train_l1_error:.4f} "
+              f"Val L1 Error: {val_l1_error:.4f} "
+              f"Train Longitudinal Error: {train_longitudinal_error:.4f} "
+              f"Val Longitudinal Error: {val_longitudinal_error:.4f} "
+              f"Train Lateral Error: {train_lateral_error:.4f} "
+              f"Val Lateral Error: {val_lateral_error:.4f}")
+
+    # Save model
+    save_model(model)
+    torch.save(model.state_dict(), log_dir / f"{model_name}.th")
+    print(f"Model saved to {log_dir / f'{model_name}.th'}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--exp_dir", type=str, default="logs")
+    parser.add_argument("--model_name", type=str, required=True)
+    parser.add_argument("--num_epoch", type=int, default=50)
+    parser.add_argument("--lr", type=float, default=1e-3)
+    parser.add_argument("--seed", type=int, default=2024)
+    train(**vars(parser.parse_args()))
+
+# print("Time to train")
